181b4585f7d8a555d97680e06e41d8a268bc8eae
Ju <ju.zhang.jz1@roche.com>
2020-06-09 17:46:32 +0000

    some updated

--- summarization/bart/run_train_tiny.sh
+++ summarization/bart/run_train_tiny.sh
@@ -20,14 +20,14 @@ mkdir -p $OUTPUT_DIR
 # Add parent directory to python path to access lightning_base.py and utils.py
 export PYTHONPATH="../../":"${PYTHONPATH}"
 python finetune.py \
---data_dir=cnn_tiny/ \
---model_name_or_path=sshleifer/bart-tiny-random \
---learning_rate=1e-3 \
+--data_dir=train_test_data/ \
+--model_name_or_path=facebook/bart-large-cnn \
+--learning_rate=1e-2 \
 --train_batch_size=2 \
 --eval_batch_size=2 \
 --output_dir=$OUTPUT_DIR \
---num_train_epochs=150  \
---n_gpu=0 \
+--num_train_epochs=200  \
+--n_gpu=4  \
 --do_train \
 --do_predict $@
 
--- summarization/bart/test_test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-echo "While I thought that I was learning how to live, I have been learning how to die." > quote1.txt
