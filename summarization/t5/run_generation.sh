99b628ab165bf0929f23f85eba97023bbf83a547
Ju <ju.zhang.jz1@roche.com>
2020-06-10 20:28:27 +0000

    move result around

--- summarization/t5/run_generation.sh
+++ summarization/t5/run_generation.sh
@@ -10,10 +10,10 @@ mkdir -p $OUTPUT_DIR
 # Add parent directory to python path to access lightning_base.py and utils.py
 export PYTHONPATH="../../":"${PYTHONPATH}"
 python generate_summary.py \
---model_size=$OUTPUT_DIR/ \
+--model_size=$OUTPUT_DIR \
 --input_path=$CURRENT_DIR/all_train_test_data/test.source \
---output_path=$OUTPUT_DIR/t5_fine_tuned_generated_title.text \
---score_path=$OUTPUT_DIR/t5_fine_tuned_rouge_score.txt \
+--output_path=$OUTPUT_DIR/t5small_fine_tuned_generated_title.text \
+--score_path=$OUTPUT_DIR/t5small_fine_tuned_rouge_score.txt \
 --reference_path=$CURRENT_DIR/all_train_test_data/test.target 
 
 #model_size, input_path, output_path, reference_path, score_pathinput_path
