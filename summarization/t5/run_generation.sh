cc692fb5807799cf37cc26913e74d29ed7281a34
Ju <ju.zhang.jz1@roche.com>
2020-06-10 10:00:53 +0000

    test

--- summarization/t5/run_generation.sh
+++ summarization/t5/run_generation.sh
@@ -10,8 +10,13 @@ mkdir -p $OUTPUT_DIR
 # Add parent directory to python path to access lightning_base.py and utils.py
 export PYTHONPATH="../../":"${PYTHONPATH}"
 python generate_summary.py \
---source_path=all_train_test_data/test.source \
---output_path=$OUTPUT_DIR/t5_fine_tuned_generated_title.text \
+model_size=t5-small \
+input_path=all_train_test_data/test.source \
+output_path=$OUTPUT_DIR/t5_fine_tuned_generated_title.text \
+score_path=$OUTPUT_DIR/t5_fine_tuned_rouge_score.txt \
 --model_name=t5_output \
+--reference_path=all_train_test_data/test.target \
 --device=cuda \
 --bs=5 \
+
+#model_size, input_path, output_path, reference_path, score_pathinput_path
